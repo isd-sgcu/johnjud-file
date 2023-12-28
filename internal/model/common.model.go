88eea5ed8f1e188f44ce66ea1219691c1d7223dd
Idhibhat Pankam <marginpankam@gmail.com>
2023-12-28 22:51:36 +0700

    fix: gorm timestamp

--- internal/model/common.model.go
+++ internal/model/common.model.go
@@ -9,9 +9,9 @@ import (
 
 type Base struct {
 	ID        uuid.UUID      `json:"id" gorm:"primaryKey;size:191"`
-	CreatedAt time.Time      `json:"created_at" gorm:"type:datetime;autoCreateTime:nano"`
-	UpdatedAt time.Time      `json:"updated_at" gorm:"type:datetime;autoUpdateTime:nano"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index;type:datetime"`
+	CreatedAt time.Time      `json:"created_at" gorm:"type:timestamp;autoCreateTime:nano"`
+	UpdatedAt time.Time      `json:"updated_at" gorm:"type:timestamp;autoUpdateTime:nano"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index;type:timestamp"`
 }
 
 func (m *Base) BeforeCreate(_ *gorm.DB) error {
