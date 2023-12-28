f3267c80963130f64fc1b5ddf35b8db5abe4db65
Idhibhat Pankam <marginpankam@gmail.com>
2023-12-28 22:49:46 +0700

    fix: constant

--- constant/pet.constant.go
+++ constant/pet.constant.go
@@ -1,4 +1,4 @@
-package pet
+package constant
 
 type Gender int
 
--- database/postgresql.connection.go
+++ database/postgresql.connection.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/isd-sgcu/johnjud-file/src/config"
+	"github.com/isd-sgcu/johnjud-file/cfgldr"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	gormLogger "gorm.io/gorm/logger"
 )
 
-func InitPostgresDatabase(conf *config.Database, isDebug bool) (db *gorm.DB, err error) {
+func InitPostgresDatabase(conf *cfgldr.Database, isDebug bool) (db *gorm.DB, err error) {
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", conf.Host, strconv.Itoa(conf.Port), conf.Username, conf.Password, conf.Name, conf.SSL)
 
 	gormConf := &gorm.Config{}
