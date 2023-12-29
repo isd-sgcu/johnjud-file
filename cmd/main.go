8f2fbdfd0b52957fb252f6d1f8ca28d900bc3dd0
Idhibhat Pankam <marginpankam@gmail.com>
2023-12-30 00:32:20 +0700

    feat: base s3 client

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/isd-sgcu/johnjud-file/cfgldr"
+	s3Clnt "github.com/isd-sgcu/johnjud-file/pkg/client/s3"
+	"github.com/rs/zerolog/log"
+)
+
 func main() {
-	// Code
+	conf, err := cfgldr.LoadConfig()
+	if err != nil {
+		log.Fatal().
+			Err(err).
+			Str("service", "auth").
+			Msg("Failed to load config")
+	}
+
+	sdkConfig, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		fmt.Println("Couldn't load default configuration. Have you set up your AWS account?")
+		fmt.Println(err)
+		return
+	}
+	awsClient := s3.NewFromConfig(sdkConfig)
+
+	s3Client := s3Clnt.NewClient(conf.S3, awsClient)
+
+	count := 10
+	fmt.Printf("Let's list up to %v buckets for your account.\n", count)
+	result, err := awsClient.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
+	if err != nil {
+		fmt.Printf("Couldn't list buckets for your account. Here's why: %v\n", err)
+		return
+	}
+	if len(result.Buckets) == 0 {
+		fmt.Println("You don't have any buckets!")
+	} else {
+		if count > len(result.Buckets) {
+			count = len(result.Buckets)
+		}
+		for _, bucket := range result.Buckets[:count] {
+			fmt.Printf("\t%v\n", *bucket.Name)
+		}
+	}
 }
