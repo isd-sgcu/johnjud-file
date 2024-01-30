adb8a400d8c349e3f64a22351e363756fa98055a
Idhibhat Pankam <marginpankam@gmail.com>
2024-01-30 22:21:54 +0700

    fix: remove unique from petid in image

--- internal/model/image.model.go
+++ internal/model/image.model.go
@@ -6,7 +6,7 @@ import (
 
 type Image struct {
 	Base
-	PetID     *uuid.UUID `json:"pet_id" gorm:"index:idx_name,unique"`
+	PetID     *uuid.UUID `json:"pet_id" gorm:"index:idx_name"`
 	Pet       *Pet       `json:"pet" gorm:"foreignKey:PetID;constraint:OnUpdate:CASCADE;OnDelete:SET NULL;"`
 	ImageUrl  string     `json:"image_url" gorm:"mediumtext"`
 	ObjectKey string     `json:"object_key" gorm:"mediumtext"`
